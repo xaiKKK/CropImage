7b446036d792a69d93cabded6ef74d6cf41658af
UserTest <usertest@yandex.ru>
2024-02-14 11:18:19 +0300

    реализованно перемещение

--- src/Image.tsx
+++ src/Image.tsx
@@ -28,16 +28,15 @@ export interface ImageProps {
   objects: ImageObject[];
 }
 
-const Image: React.FC<{ props: ImageProps }> = ({ props }) => {
+const Image: React.FC<{ 
+  props: ImageProps; 
+  onArrowMove: (index: number, point: Point) => void; 
+  renderArrow: (object: ArrowImageObject, index: number) => JSX.Element 
+}> = ({ props, onArrowMove, renderArrow }) => {
   const { src, crop, objects } = props;
 
-  const renderArrow = (object: ArrowImageObject, index: number) => {
-    return (
-      <svg key={index} style={{ position: 'absolute', left: object.point.x, top: object.point.y }}>
-        <line x1="0" y1="0" x2="20" y2="20" stroke="white" strokeWidth="2" />
-        <text x="20" y="20">{object.comment}</text>
-      </svg>
-    );
+  const handleArrowMove = (index: number, point: Point) => {
+    onArrowMove(index, point);
   };
 
   return (
--- src/ImageEditor.tsx
+++ src/ImageEditor.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Image, { ImageProps, Rect, ArrowImageObject } from './Image';
+import Image, { ImageProps, ArrowImageObject, Point } from './Image';
 
 interface ImageEditorProps {
   props: ImageProps;
@@ -11,8 +11,6 @@ const ImageEditor: React.FC<ImageEditorProps> = ({ props, onSave }) => {
   const [cropY, setCropY] = useState(props.crop.y);
   const [cropWidth, setCropWidth] = useState(props.crop.w);
   const [cropHeight, setCropHeight] = useState(props.crop.h);
-  const [arrowX, setArrowX] = useState(0);
-  const [arrowY, setArrowY] = useState(0);
   const [objects, setObjects] = useState([...props.objects]);
 
   const handleCropChange = (field: string, value: number) => {
@@ -46,10 +44,46 @@ const ImageEditor: React.FC<ImageEditorProps> = ({ props, onSave }) => {
   const handleAddArrow = () => {
     const newArrow: ArrowImageObject = {
       type: "arrow",
-      point: { x: arrowX, y: arrowY },
-      comment: "New arrow"
+      point: { x: 0, y: 0 },
+      comment: "Нажать"
     };
     setObjects([...objects, newArrow]);
+  };
+
+  const handleArrowMove = (index: number, point: Point) => {
+    const updatedObjects = objects.map((obj, i) => (i === index ? { ...obj, point } : obj));
+    setObjects(updatedObjects);
+  };
+
+  const renderArrow = (object: ArrowImageObject, index: number) => {
+    const handleMouseDown = (e: React.MouseEvent<SVGElement>) => {
+      e.preventDefault();
+      
+      const rect = e.currentTarget.getBoundingClientRect();
+      const offsetX = e.clientX - rect.left;
+      const offsetY = e.clientY + 101 - rect.top;
+
+      const onMouseMove = (e: MouseEvent) => {
+        handleArrowMove(index, { x: e.clientX - cropX - offsetX, y: e.clientY - cropY - offsetY });
+      };
+      const onMouseUp = () => {
+        window.removeEventListener('mousemove', onMouseMove);
+        window.removeEventListener('mouseup', onMouseUp);
+      };
+      window.addEventListener('mousemove', onMouseMove);
+      window.addEventListener('mouseup', onMouseUp);
+    };
+
+    return (
+      <svg
+        key={index}
+        style={{ position: 'absolute', left: object.point.x, top: object.point.y, cursor: 'move' }}
+        onMouseDown={handleMouseDown}
+      >
+        <line x1="0" y1="0" x2="20" y2="20" stroke="white" strokeWidth="2" />
+        <text x="20" y="20">{object.comment}</text>
+      </svg>
+    );
   };
 
   return (
@@ -70,16 +104,8 @@ const ImageEditor: React.FC<ImageEditorProps> = ({ props, onSave }) => {
         <label>Height:</label>
         <input type="number" value={cropHeight} onChange={(e) => handleCropChange("height", parseInt(e.target.value))} />
       </div>
-      <div>
-        <label>Arrow X:</label>
-        <input type="number" value={arrowX} onChange={(e) => setArrowX(parseInt(e.target.value))} />
-      </div>
-      <div>
-        <label>Arrow Y:</label>
-        <input type="number" value={arrowY} onChange={(e) => setArrowY(parseInt(e.target.value))} />
-      </div>
       <button onClick={handleAddArrow}>Add Arrow</button>
-      <Image props={{ ...props, crop: { x: cropX, y: cropY, w: cropWidth, h: cropHeight }, objects }} />
+      <Image props={{ ...props, crop: { x: cropX, y: cropY, w: cropWidth, h: cropHeight }, objects }} onArrowMove={handleArrowMove} renderArrow={renderArrow} />
       <button onClick={handleSave}>Save</button>
     </div>
   );
